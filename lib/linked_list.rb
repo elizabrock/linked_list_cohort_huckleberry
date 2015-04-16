ea8411c2bc9240107422d8ba792b57a0e983cdde
Eliza Brock <eliza@elizabrocksoftware.com>
2015-04-16 10:25:42 -0500

    Refactor variable names.

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -2,24 +2,24 @@ require_relative 'linked_list_item'
 
 class LinkedList
 
-  def get(nth_item)
-    if nth_item == 0
+  def get(index)
+    if index == 0
       @first_item.payload
     else
       current_node = @first_item
-      nth_item.times do
+      index.times do
         current_node = current_node.next_item
       end
       current_node.payload
     end
   end
 
-  def push(item)
+  def push(payload)
     if @first_item.nil?
-      @first_item = LinkedListItem.new(item)
+      @first_item = LinkedListItem.new(payload)
       @last_item = @first_item
     else
-      @last_item.next_item = LinkedListItem.new(item)
+      @last_item.next_item = LinkedListItem.new(payload)
       @last_item = @last_item.next_item
     end
   end
