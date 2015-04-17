cc147f1dba1c6911997532a12b2d0d7000be7172
Eliza Brock <eliza@elizabrocksoftware.com>
2015-04-17 09:35:54 -0500

    Contrast the two different approaches to push.

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -15,12 +15,21 @@ class LinkedList
   end
 
   def push(payload)
+    new_item = LinkedListItem.new(payload)
     if @first_item.nil?
-      @first_item = LinkedListItem.new(payload)
-      @last_item = @first_item
+      @first_item = new_item
     else
-      @last_item.next_item = LinkedListItem.new(payload)
-      @last_item = @last_item.next_item
+
+      # Eliza's "Naive" Implementation:
+      # last_item = @first_item
+      # until last_item.last? # i.e. last_item.next_item.nil?
+      #   last_item = last_item.next_item
+      # end
+      # last_item.next_item = new_item
+
+      # Jurnell's Implementation:
+      @last_item.next_item = new_item
     end
+    @last_item = new_item
   end
 end
